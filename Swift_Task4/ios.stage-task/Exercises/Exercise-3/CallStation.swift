2d8cb8c00473e6acb7ffd2650e7e98df3946bb8c
vwf <vwff@icloud.com>
2021-06-06 18:20:47 +0500

    fix: delete task 3

--- Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
@@ -2,7 +2,7 @@ import Foundation
 
 final class CallStation {
     var usersSet: Set<User> = []
-    var calls: [CallID: Call] = [:]
+    var callsLog: [CallID: Call] = [:]
     var currentCalls: [UUID: Call] = [:]
 }
 
@@ -24,47 +24,48 @@ extension CallStation: Station {
     }
     
     func execute(action: CallAction) -> CallID? {
-        switch action {
-        case let .start(from: fromUser, to: toUser):
-            guard usersSet.contains(fromUser) else {
-                return nil
-            }
-            
-            guard usersSet.contains(toUser) else {
-                let call = Call(id: CallID(), incomingUser: fromUser, outgoingUser: toUser, status: .ended(reason: .error))
-                calls[call.id] = call
-                currentCalls[fromUser.id] = nil
-                currentCalls[toUser.id] = nil
-                return call.id
-            }
-            
-            guard currentCall(user: toUser) == nil else {
-                    let call = Call(id: CallID(), incomingUser: fromUser, outgoingUser: toUser, status: .ended(reason: .userBusy))
-                    calls[call.id] = call
-                    return call.id
-                }
-            let call = Call(id: CallID(), incomingUser: fromUser, outgoingUser: toUser, status: .calling)
-            calls[call.id] = call
-            currentCalls[fromUser.id] = call
-            currentCalls[toUser.id] = call
-            return call.id
-            
-        case let .answer(from: toUser):
-            guard let call = currentCall(user: toUser) else {
-                return nil
-            }
-            
-            let fromUser = call.incomingUser
-            if !usersSet.contains(fromUser) || !usersSet.contains(toUser) {
-                            let call = Call(id: CallID(), incomingUser: fromUser, outgoingUser: toUser, status: .ended(reason: .error))
-                            calls[call.id] = call
-                            currentCalls[fromUser.id] = nil
-                            currentCalls[toUser.id] = nil
-                            return nil
-                        }
-            
-        
-        }
+//        switch action {
+//        case let .start(from: fromUser, to: toUser):
+//            guard usersSet.contains(fromUser) else {
+//                return nil
+//            }
+//
+//            guard usersSet.contains(toUser) else {
+//                let call = Call(id: CallID(), incomingUser: fromUser, outgoingUser: toUser, status: .ended(reason: .error))
+//                calls[call.id] = call
+//                currentCalls[fromUser.id] = nil
+//                currentCalls[toUser.id] = nil
+//                return call.id
+//            }
+//
+//            guard currentCall(user: toUser) == nil else {
+//                    let call = Call(id: CallID(), incomingUser: fromUser, outgoingUser: toUser, status: .ended(reason: .userBusy))
+//                    calls[call.id] = call
+//                    return call.id
+//                }
+//            let call = Call(id: CallID(), incomingUser: fromUser, outgoingUser: toUser, status: .calling)
+//            calls[call.id] = call
+//            currentCalls[fromUser.id] = call
+//            currentCalls[toUser.id] = call
+//            return call.id
+//
+//        case let .answer(from: toUser):
+//            guard let call = currentCall(user: toUser) else {
+//                return nil
+//            }
+//
+//            let fromUser = call.incomingUser
+//            if !usersSet.contains(fromUser) || !usersSet.contains(toUser) {
+//                            let call = Call(id: CallID(), incomingUser: fromUser, outgoingUser: toUser, status: .ended(reason: .error))
+//                            calls[call.id] = call
+//                            currentCalls[fromUser.id] = nil
+//                            currentCalls[toUser.id] = nil
+//                            return nil
+//                        }
+//
+//
+//        }
+        return nil
     }
     
     func calls() -> [Call] {
